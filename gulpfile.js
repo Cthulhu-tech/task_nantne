aeec22579260d721ffeb559738d19e4b58218069
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-01-30 01:36:30 +0400

    heroky problem resolve

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const sass  = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps'); // для удобства просмотра кода css
+
+gulp.task('sass', async () => {
+
+    gulp.src('./dist/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./src/'));
+
+})
+
+gulp.task('pug', async (done) => {
+
+    gulp.src('./dist/**/*.pug')
+    .pipe(pug({pretty: true}))
+    .pipe(gulp.dest('./src/'));
+
+    done();
+})
+
+gulp.task('run',gulp.series('sass','pug'))
+
+gulp.task('watch',() => {
+    gulp.watch('./dist/**/*.scss', gulp.series('sass'));
+    gulp.watch('./dist/**/*.pug', gulp.series('pug'));
+});
+
+gulp.task('default', gulp.series( 'watch'));
